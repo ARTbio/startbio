a0308ab378ce37064c9d456352612be93e36d411
Christophe Antoniewski <drosofff@users.noreply.github.com>
2019-11-06 00:34:25 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@ mkdir mkdocs_build
 cd mkdocs_build
 # Initialize gh-pages checkout
 DATE=`date`
-git clone https://github.com/ARTbio/springday.git
-cd springday
+git clone https://github.com/ARTbio/startbio.git
+cd startbio
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
-# cd $SCRIPT_PATH
