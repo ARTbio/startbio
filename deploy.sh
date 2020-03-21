10a1dad93994eb6f0ca83bad51c1b0b9ab94aefe
Christophe Antoniewski <drosofff@users.noreply.github.com>
2020-03-21 17:58:39 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ cd mkdocs_build
 DATE=`date`
 git clone https://github.com/ARTbio/startbio.git
 cd startbio
+shuf docs/Covid-19/youtube.md -o shuffle.txt
+mv shuffle.txt docs/Covid-19/youtube.md
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
