1d29f97cb1613220c8be184ab399fb0a3f4fbaad
bellenger-l <bellenger_lea_31@hotmail.fr>
2023-09-27 17:59:22 +0200

    add the mandatory js function for spoiler

--- docs/spoiler_files/spoiler.js
+++ docs/spoiler_files/spoiler.js
@@ -1,3 +1,8 @@
+function toggleSpoiler(element) {
+    var hiddenText = element.nextElementSibling;
+    hiddenText.style.display = "inline";
+  }
+  
 document.addEventListener("DOMContentLoaded", function() {
     var spoilerElements = document.getElementsByClassName("spoiler");
     for (var i = 0; i < spoilerElements.length; i++) {
